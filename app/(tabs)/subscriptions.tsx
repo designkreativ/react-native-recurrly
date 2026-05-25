405b8b5e27910f3a1d18a7b4447bedfc0de625c3
designkreativ <dmmk.creations.21@gmail.com>
2026-05-25 15:11:37 +0300

    feat: project setup,nativewind, routing, tab navigation

---
+++ app/(tabs)/subscriptions.tsx
@@ -0,0 +1,17 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+import { styled } from "nativewind";
+import { SafeAreaView  as RNSafeAreaView} from "react-native-safe-area-context";
+
+const SafeAreaView = styled(RNSafeAreaView)
+
+const Subscriptions = () => {
+  return (
+    <SafeAreaView className="flex-1 items-center justify-center bg-background p-5">
+      <Text>Subscriptions</Text>
+    </SafeAreaView>
+  )
+}
+
+export default Subscriptions
